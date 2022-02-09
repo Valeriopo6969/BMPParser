e247cd7d78a122aec0e741c0303a5bda209c8e22
Valeriopo_02 <78092776+Valeriopo6969@users.noreply.github.com>
2022-02-09 18:17:50 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
-clang.exe -o .\bin\example.exe -I .\core\include -I .\SDL2-2.0.20\include\ -L .\SDL2-2.0.20\lib\x64\ .\app\src\example.c  -Xlinker /subsystem:console -lSDL2main -lSDL2 -lshell32 && .\bin\example.exe
+mkdir bin
+clang.exe -o .\bin\example.exe ^
+-I .\core\include -I .\SDL2-2.0.20\include\ ^
+-L .\SDL2-2.0.20\lib\x64\ .\app\src\example.c  ^
+-Xlinker /subsystem:console -lSDL2main -lSDL2 -lshell32 && .\bin\example.exe
